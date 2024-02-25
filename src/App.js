d02ecaa8fd2e88e30eb8ff9d32e41f8a6df7c2dc
Kevin Fahrenberger <fahrenberger.k@web.de>
2024-02-25 12:58:22 +0100

    feat: 3 vertical logic added

--- src/App.js
+++ src/App.js
@@ -13,6 +13,18 @@ const tileColors = [greenTile, blueTile, purpleTile, orangeTile];
 
 const App = () => {
   const [grid, setGrid] = useState([]);
+
+  const checkForVerticalThree = () => {
+    for (let i = 0; i < width * (height - 2); i++) {
+   const verticalThree = [i, i + width, i + width * 2];
+   const checkColor = grid[i];
+   if ( verticalThree.every(number => grid[number] === checkColor) ) {
+    verticalThree.forEach(number => grid[number] = "");
+   }
+   
+    }
+  }
+
   const createGrid = () => {
     const grid = [];
     for (let i = 0; i < width * height; i++) {
@@ -26,7 +38,10 @@ const App = () => {
   useEffect(() => {
     createGrid();
   }, []);
-  // createGrid();
+  
+  useEffect(() => {
+    checkForVerticalThree();
+  }, [grid]);
 
   return (
     <div className="app">
