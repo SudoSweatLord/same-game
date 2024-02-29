e22d6ab9455306ffc6e6678e6ed9de7b0b22cf90
Kevin Fahrenberger <fahrenberger.k@web.de>
2024-02-29 05:04:26 +0100

    fix: background import

--- src/Background.js
+++ src/Background.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import background from "./resources/background.mp4";
 function Background() {
   return (
     <video id="background-video" autoPlay loop muted poster="">
