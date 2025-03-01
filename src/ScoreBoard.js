5de1d0d3dd9035ec46d06d926529a4418b124c74
K-Fahrenberger <fahrenberger.k@web.de>
2025-03-01 17:49:14 +0100

    fix: rescaling of scoreboard when increasing

--- src/ScoreBoard.js
+++ src/ScoreBoard.js
@@ -3,7 +3,8 @@ import React from "react";
 const ScoreBoard = ({ score, topScores }) => {
   return (
     <div className="score-container">
-      <h4>Score: {score}</h4>
+      <h4>Score:
+        <br></br> {score}</h4>
       <h4>High Scores:</h4>
       <ul>
         {topScores.map((score, index) => (
